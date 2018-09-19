2a05f19bfff30fdf487661e4260b951307fa29fd
Sander Spies <sandermail@gmail.com>
2018-09-19 13:20:07 +0200

    Move to env record and initial setup for storing locals.

--- asmcomp/wasm32/typed_cmm.ml
+++ asmcomp/wasm32/typed_cmm.ml
@@ -54,6 +54,8 @@ type block =
 
 type block_stack = block Stack.t
 
+type local = string * machtype option
+
 let oper_result_type = function
   | Capply ty -> ty
   | Cextcall(_s, ty, _alloc, _) -> ty
@@ -76,16 +78,31 @@ let oper_result_type = function
   | Craise _ -> typ_int
   | Ccheckbound -> typ_void
 
-let return_required = true
-let no_return_required = false
+type env = {
+    stack: stack;
+    block_stack: block_stack;
+    needs_return: bool;
+    locals: local list;
+}
 
-(* let current_no_of_types = ref 0
-let check_types msg add_types =
-    current_no_of_types := !current_no_of_types + add_types;
-    if current_no_of_types <> Stack.length stack then
-        failwith msg *)
+let initial_env () = 
+  let stack : stack = Stack.create () in
+  let block_stack : block_stack = Stack.create() in
+  let locals : local list = [] in
+  Stack.push Bfunction block_stack;
+  { 
+    stack; 
+    block_stack; 
+    needs_return = true; 
+    locals 
+  }
 
-let rec process stack block_stack needs_return = function
+let rec process env e = 
+  let stack = env.stack in
+  let block_stack = env.block_stack in
+  let needs_return = env.needs_return in
+  let locals = env.locals in
+  match e with 
     | Cconst_int i -> 
         Stack.push typ_int stack;
         Tconst_int i
@@ -96,76 +113,97 @@ let rec process stack block_stack needs_return = function
         Stack.push typ_float stack;
         Tconst_float f
     | Cconst_symbol s -> 
+        print_endline s;
         Stack.push typ_int stack; (* potentially incorrect *)
         Tconst_symbol s
     | Cblockheader (n, d) -> 
         Stack.push typ_int stack;
         Tblockheader (n, d)
     | Cvar i -> (* potentially incorrect *)
+        print_endline (i.Ident.name ^ "/" ^ string_of_int i.Ident.stamp);
         Stack.push typ_int stack;
-        Tvar i
+        Tvar i    
     | Clet (i, r, b) -> 
-        let r = process stack block_stack return_required r in      
+        let r = process {env with needs_return = true} r in      
         ignore(Stack.pop stack);
-        let result = Tlet (i, r, process stack block_stack return_required b) in
-        result
+        Tlet (i, r, process env b)        
     | Cassign (i, e) -> 
-        let result = Tassign (i, process stack block_stack no_return_required e) in
+        let result = Tassign (i, process {env with needs_return = false} e) in
         ignore(Stack.pop stack);
+        Stack.push typ_void stack;
         result
+    | Ctuple [] -> 
+        Stack.push typ_void stack;
+        Ttuple []
     | Ctuple el ->         
         let result = Ttuple (List.mapi (fun i e -> 
             (if i > 0 then 
                 ignore(Stack.pop stack);
-            process stack block_stack needs_return e)) el) in
-        result
+            process env e)) el) in
+        result 
     | Cop (o, el, d) ->
         let r = oper_result_type o in    
         let expected_length = Stack.length stack + List.length el in
-        let processed = List.map (process stack block_stack return_required) el in        
+        let processed = List.map (process {env with needs_return = true}) el in        
         let result = Top (o, processed, d, r) in         
         (if Stack.length stack <> expected_length then
             failwith ("Not correct:" ^ string_of_int (Stack.length stack) ^ " vs " ^ string_of_int expected_length)
         );
         List.iter (fun _ -> ignore(Stack.pop stack)) el;
-        Stack.push r stack; (* typ_void causes challenges *)
+        if needs_return then print_endline "needs a return..."
+        else print_endline "no return needed";
+        Stack.push r stack;
         result
-    | Csequence (f, Ctuple []) ->        
-        let processed_f = process stack block_stack return_required f in
+    | Csequence (f, Cconst_int 1) -> 
+        print_endline "mkay!";
+        let before = Stack.length stack in
+        let processed_f = process {env with needs_return = false} f in
         ignore(Stack.pop stack);
+        assert (Stack.length stack = before);
+        Stack.push typ_int stack;
+        Tsequence (processed_f, Tconst_int 1)
+    | Csequence (f, Ctuple []) ->
+        let before = Stack.length stack in
+        let processed_f = process {env with needs_return = true} f in
+        ignore(Stack.pop stack);
+        assert (Stack.length stack = before);
+        Stack.push typ_void stack;
         Tsequence (processed_f, Ttuple [])
     | Csequence (Clet _ as f, s) ->
-        let a = Stack.length stack in
-        let processed_f = process stack block_stack no_return_required f in    
-        if a <> Stack.length stack then
-            ignore(Stack.pop stack);
-        Tsequence (processed_f, process stack block_stack needs_return s)
-    | Csequence (Ctuple [], s) ->
-        Tsequence (Ttuple [], process stack block_stack needs_return s)        
-    | Csequence (f, s) -> 
-        (* print_endline "at"  ; *)
-        let processed_f = process stack block_stack return_required f in
+        let before = Stack.length stack in
+        let processed_f = process {env with needs_return = false} f in    
         ignore(Stack.pop stack);
-        Tsequence (processed_f, process stack block_stack needs_return s)
-    | Cifthenelse (i, t, e) ->        
+        assert (Stack.length stack = before);
+        Tsequence (processed_f, process env s)
+    | Csequence (Ctuple [], s) ->
+        Tsequence (Ttuple [], process env s)        
+    | Csequence (f, s) -> 
+        let before = Stack.length stack in
+        let processed_f = process env f in        
+        ignore(Stack.pop stack);
+        assert (Stack.length stack = before);
+        Tsequence (processed_f, process env s)
+    | Cifthenelse (i, t, e) ->            
         Stack.push Bifthenelse block_stack;
         let before_stack_length = Stack.length stack in 
-        let processed_i = process stack block_stack return_required i in  
+        let processed_i = process env i in  
         ignore(Stack.pop stack);      
         let t_stack = Stack.copy stack in
-        let processed_t = process t_stack (Stack.copy block_stack) needs_return t in
+        let t_env = {
+          stack = t_stack; 
+          block_stack = (Stack.copy block_stack); 
+          needs_return; 
+          locals
+        } in
+        let processed_t = process t_env t in
         let e_stack = Stack.copy stack in
-        let processed_e = process e_stack (Stack.copy block_stack) needs_return e in
-        (if Stack.length t_stack > Stack.length e_stack then 
-            match e with 
-            | Cop (Calloc, _, _) -> Stack.push (Stack.top t_stack) e_stack
-            | _ -> ()
-        );
-        (if Stack.length t_stack < Stack.length e_stack then 
-            match t with 
-            | Cop (Calloc, _, _) -> Stack.push (Stack.top e_stack) t_stack
-            | _ -> ()
-        );
+        let e_env = {
+          stack = e_stack; 
+          block_stack = (Stack.copy block_stack); 
+          needs_return; 
+          locals
+        } in
+        let processed_e = process e_env e in        
         let then_type = Stack.top t_stack in
         let else_type = Stack.top e_stack in
         if not (compare then_type else_type) then (
@@ -173,40 +211,38 @@ let rec process stack block_stack needs_return = function
             print_stack_type else_type;
             failwith "Then and else need to return the same type";
         );
-
-        let rt = if (before_stack_length = Stack.length t_stack) then  
-            Stack.top stack
-         else 
-            (print_endline "void xxx";
-            typ_void)
-        in
+        let rt = if Stack.length t_stack > Stack.length stack then Stack.top t_stack else Stack.top e_stack in
         let result = Tifthenelse (processed_i, processed_t, processed_e, rt) in    
             
         Stack.push rt stack;
         ignore(Stack.pop block_stack);
+        assert (Stack.length stack = before_stack_length + 1);
         result
     | Cswitch (e, ia, ea, d) -> 
-        (* let check = Stack.length stack in *)
-        let result = Tswitch (process stack block_stack true (* not sure *) e, ia, Array.map (process stack block_stack needs_return) ea, d) in
+        let check = Stack.length stack in
+        let copied_stack = Stack.copy stack in
+        let copied_block_stack = Stack.copy block_stack in
+        let copied_env = {stack = copied_stack; block_stack = copied_block_stack; needs_return; locals} in
+        let result = Tswitch (process {copied_env with needs_return = true} (* not sure *) e, ia, Array.map (process copied_env) ea, d) in
         let switch_result = ref typ_void in
         Array.iter (fun _ -> 
-            let item = Stack.pop stack in 
+            let item = Stack.pop copied_stack in 
             switch_result := item
         ) ea;         
         Stack.push !switch_result stack;
-        (* assert (check = Stack.length stack - 1); *)
+        assert (check = Stack.length stack - 1);
         result
     | Cloop e -> 
         Stack.push Bloop block_stack;
-        let result = Tloop (process stack block_stack needs_return e) in
+        let result = Tloop (process env e) in
         ignore(Stack.pop block_stack);
         result
     | Ccatch (r, with_, body_) -> 
         let ccatch_stack = Stack.copy stack in
         let ccatch_block_stack = Stack.copy block_stack in
+        let ccatch_env = { stack = ccatch_stack; block_stack = ccatch_block_stack; needs_return; locals } in
         let with_exprs = List.map(fun (i, il, expr) -> (
-          print_endline (string_of_int (List.length il));
-          let result = (i, il, process ccatch_stack ccatch_block_stack needs_return expr) in
+          let result = (i, il, process ccatch_env expr) in
           Stack.push (Bwith (i, Stack.top ccatch_stack)) ccatch_block_stack;
           result
         )) with_
@@ -214,32 +250,45 @@ let rec process stack block_stack needs_return = function
         let result = Tcatch (
           r, 
           with_exprs, 
-          (let result = process ccatch_stack ccatch_block_stack needs_return body_ in 
+          (let result = process ccatch_env body_ in 
            result)) 
         in
         if Stack.length ccatch_stack > Stack.length stack then
             Stack.push (Stack.top ccatch_stack) stack;
         result
     | Cexit (i, el) -> 
+        print_endline ("-> exit " ^ string_of_int i);
+        let found = ref false in
         Stack.iter (fun f -> 
             match f with 
-            | Bwith (i, t) when i = i -> 
-                (print_string "okay:";
-                 print_stack_type t;
-                Stack.push t stack)
+            | Bwith (ix, t) when ix = i -> 
+                (found := true;
+                 Stack.push t stack)
             | _ -> ()
         ) block_stack;
-        let result = Texit (i, List.map (process stack block_stack needs_return) el) in
+        assert !found;
+        print_endline "cexit end";
+        let result = Texit (i, List.map (process env) el) in
         List.iter (fun _ -> ignore(Stack.pop stack)) el;
         result
-    | Ctrywith (e, i, c) -> Ttrywith (process stack block_stack needs_return e, i, process (Stack.copy stack) (Stack.copy block_stack) needs_return c)
+    | Ctrywith (e, i, c) -> 
+        let stack_size_before = Stack.length stack in
+        let copied_env = {
+          stack = (Stack.copy stack); 
+          block_stack = (Stack.copy block_stack); 
+          needs_return; 
+          locals
+        } in
+        let result = Ttrywith (process env e, i, process copied_env c) in
+        assert (stack_size_before = Stack.length stack - 1);
+        result
 
 let add_types func_name e =
-    print_endline ("Adding types to function:" ^ func_name);
-    let stack : stack = Stack.create () in
-    let block_stack : block_stack = Stack.create() in
-    Stack.push Bfunction block_stack;
-    let result = process stack block_stack true e in
+    print_endline ("Adding types to function:" ^ func_name);    
+    let env = initial_env() in
+    let stack = env.stack in
+    let block_stack = env.stack in
+    let result = process env e in
     print_endline "finished";
     (if (Stack.length stack <> 1) then (
         Stack.iter print_stack_type stack;
