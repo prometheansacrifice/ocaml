4af0a58d96a2259ff96f11655e3e5e063c84d092
Manas <prometheansacrifice@gmail.com>
2018-11-04 21:00:31 +0530

    Add .travis-ci-wasm32.sh

---
+++ .travis-ci-wasm32.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPO_DIR=$PWD
+
+git clone https://github.com/sanderspies/llvm llvmwasm
+cd llvmwasm/llvm/tools
+git clone https://github.com/sanderspies/lld 
+cd ../../
+
+mkdir llvm-build
+cd llvm-build
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_TARGETS_TO_BUILD= -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly ../llvm 
+make -j 6
+cd  ../../
+
+git clone --recursive https://github.com/sanderspies/wabt
+cd wabt
+make
+cd ../
+
+export LLVM_HOME=$PWD/llvmwasm/llvm-build
+./configure -no-pthread -no-debugger -no-curses -no-ocamldoc -no-graph -target-wasm32
+make coldstart
+./wabt/bin/wasm2wat --help
+make wasm32
+make wasm32-test
