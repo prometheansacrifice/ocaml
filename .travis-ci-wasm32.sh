518d2bacead2cc7cfd18ded37e16261b9a5bb92e
Manas <prometheansacrifice@gmail.com>
2018-11-04 21:22:28 +0530

    wip

--- .travis-ci-wasm32.sh
+++ .travis-ci-wasm32.sh
@@ -5,24 +5,25 @@ REPO_DIR=$PWD
 mkdir -p llvmwasm
 cd  llvmwasm
 git clone https://github.com/sanderspies/llvm
-ls
+cd $REPO_DIR
+
 mkdir -p llvmwasm/llvm/tools
 cd llvmwasm/llvm/tools
 git clone https://github.com/sanderspies/lld 
-cd ../../
+cd $REPO_DIR
 
 mkdir llvm-build
 cd llvm-build
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_TARGETS_TO_BUILD= -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly ../llvm 
 make -j 6
-cd  ../../
+cd $REPO_DIR
 
 git clone --recursive https://github.com/sanderspies/wabt
 cd wabt
 make
 cd ../
 
-export LLVM_HOME=$PWD/llvmwasm/llvm-build
+export LLVM_HOME=$REPO_DIR/llvmwasm/llvm-build
 ./configure -no-pthread -no-debugger -no-curses -no-ocamldoc -no-graph -target-wasm32
 make coldstart
 ./wabt/bin/wasm2wat --help
